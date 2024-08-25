9e85956fc4485377747464a54b65539194194312
Alexander <lyannoy.alexander@gmail.com>
2024-08-25 12:34:26 +0200

    Remove camera "delta velocity", move input handle to tick.

--- Source/SandboxEngine/World/Camera.cpp
+++ Source/SandboxEngine/World/Camera.cpp
@@ -1,33 +1,54 @@
 #include "sndpch.h"
 #include "SandboxEngine/World/Camera.h"
+#include "SandboxEngine/Core/Input.h"
 #include <glm/gtx/rotate_vector.hpp>
 
-snd::Camera::Camera(Camera::Type type)
-	: m_Type(type), m_Proxy()
+snd::Camera::Camera(Type type)
+	: m_Type(type)
 {
 	m_Proxy.Up = glm::vec3(0.0f, 1.0f, 0.0f);
 }
 
 void snd::Camera::Tick(float dt)
 {
-	if (m_MoveStraightDelta != 0.0f)
+	if (input::IsButtonDown(input::KeyboardBit::A))
 	{
-		MoveStraight(m_MoveStraightDelta);
+		MoveRight(-1.0f);
 	}
 
-	if (m_MoveSidewayDelta != 0.0f)
+	if (input::IsButtonDown(input::KeyboardBit::D))
 	{
-		MoveSideway(m_MoveSidewayDelta);
+		MoveRight(1.0f);
 	}
 
-	if (m_RotateHorizontallyAngle != 0.0f)
+	if (input::IsButtonDown(input::KeyboardBit::S))
 	{
-		RotateHorizontally(m_RotateHorizontallyAngle);
+		MoveForward(-1.0f);
 	}
 
-	if (m_RotateVerticallyAngle != 0.0f)
+	if (input::IsButtonDown(input::KeyboardBit::W))
 	{
-		RotateVertically(m_RotateVerticallyAngle);
+		MoveForward(1.0f);
+	}
+
+	if (input::IsButtonDown(input::KeyboardBit::Left))
+	{
+		RotateRight(-1.0f);
+	}
+
+	if (input::IsButtonDown(input::KeyboardBit::Right))
+	{
+		RotateRight(1.0f);
+	}
+
+	if (input::IsButtonDown(input::KeyboardBit::Down))
+	{
+		RotateUp(-1.0f);
+	}
+
+	if (input::IsButtonDown(input::KeyboardBit::Up))
+	{
+		RotateUp(1.0f);
 	}
 }
 
@@ -80,17 +101,17 @@ void snd::Camera::SetOrthographic(const glm::vec4& ortho, float n, float f)
 	SetOrthographic(ortho[0], ortho[1], ortho[2], ortho[3], n, f);
 }
 
-const glm::vec3& snd::Camera::GetLocation() const
+glm::vec3 snd::Camera::GetLocation() const
 {
 	return m_Proxy.Eye;
 }
 
-const glm::vec3& snd::Camera::GetTarget() const
+glm::vec3 snd::Camera::GetTarget() const
 {
 	return m_Proxy.At;
 }
 
-const glm::vec3& snd::Camera::GetForwardVector()
+glm::vec3 snd::Camera::GetForwardVector()
 {
 	if (m_ForwardVector.IsDirty())
 	{
@@ -101,7 +122,7 @@ const glm::vec3& snd::Camera::GetForwardVector()
 	return m_ForwardVector.Get();
 }
 
-const glm::vec3& snd::Camera::GetRightVector()
+glm::vec3 snd::Camera::GetRightVector()
 {
 	if (m_RightVector.IsDirty())
 	{
@@ -112,7 +133,7 @@ const glm::vec3& snd::Camera::GetRightVector()
 	return m_RightVector.Get();
 }
 
-const glm::mat4& snd::Camera::GetViewMatrix()
+glm::mat4 snd::Camera::GetViewMatrix()
 {
 	if (m_ViewMatrix.IsDirty())
 	{
@@ -123,20 +144,20 @@ const glm::mat4& snd::Camera::GetViewMatrix()
 	return m_ViewMatrix.Get();
 }
 
-const glm::mat4& snd::Camera::GetProjectionMatrix()
+glm::mat4 snd::Camera::GetProjectionMatrix()
 {
 	if (m_ProjectionMatrix.IsDirty())
 	{
 		switch (m_Type)
 		{
 		case Type::Orthographic:
-			//m_ProjectionMatrix = m_Proxy.OrthographicProjectionMatrix();
+			m_ProjectionMatrix = m_Proxy.OrthographicProjectionMatrix();
 			break;
 		case Type::Perspective:
 			m_ProjectionMatrix = m_Proxy.PerpsectiveProjectionMatrix();
 			break;
 		default:
-			SND_LOG_ERROR("Unknown camera type {}", (int32_t)m_Type);
+			SND_LOG_ERROR("Unknown camera type {}", static_cast<int32_t>(m_Type));
 		}
 
 		m_ProjectionMatrix.ClearDirty();
@@ -145,28 +166,28 @@ const glm::mat4& snd::Camera::GetProjectionMatrix()
 	return m_ProjectionMatrix.Get();
 }
 
-void snd::Camera::MoveStraight(float delta)
+void snd::Camera::MoveForward(float delta)
 {
 	const glm::vec3 forward = GetForwardVector();
 	SetLocation(GetLocation() + forward * delta);
 	SetTarget(GetTarget() + forward * delta);
 }
 
-void snd::Camera::MoveSideway(float delta)
+void snd::Camera::MoveRight(float delta)
 {
 	const glm::vec3 right = GetRightVector();
 	SetLocation(GetLocation() + right * delta);
 	SetTarget(GetTarget() + right * delta);
 }
 
-void snd::Camera::RotateHorizontally(float angle)
+void snd::Camera::RotateUp(float angle)
 {
-	const glm::vec3 rotateDirection = glm::rotateY(GetForwardVector(), glm::radians(-angle));
+	const glm::vec3 rotateDirection = glm::rotate(GetForwardVector(), glm::radians(angle), m_Proxy.RightVector());
 	SetTarget(GetLocation() + rotateDirection);
 }
 
-void snd::Camera::RotateVertically(float angle)
+void snd::Camera::RotateRight(float angle)
 {
-	const glm::vec3 rotateDirection = glm::rotate(GetForwardVector(), glm::radians(angle), m_Proxy.RightVector());
+	const glm::vec3 rotateDirection = glm::rotateY(GetForwardVector(), glm::radians(-angle));
 	SetTarget(GetLocation() + rotateDirection);
 }
--- Source/SandboxEngine/World/Camera.h
+++ Source/SandboxEngine/World/Camera.h
@@ -8,14 +8,14 @@ namespace snd
 	class Camera
 	{
 	public:
-		enum class Type
+		enum class Type : uint8_t
 		{
 			Orthographic,
 			Perspective,
 		};
 
 	public:
-		Camera(Camera::Type type);
+		Camera(Type type);
 
 		void Tick(float dt);
 
@@ -26,39 +26,30 @@ namespace snd
 		void SetOrthographic(float l, float r, float b, float t, float n, float f);
 		void SetOrthographic(const glm::vec4& ortho, float n, float f); // ortho should be as follows - [l, r, b, t]
 
-		const glm::vec3& GetLocation() const;
-		const glm::vec3& GetTarget() const; const
+		glm::vec3 GetLocation() const;
+		glm::vec3 GetTarget() const;
 
-		const glm::vec3& GetForwardVector();
-		const glm::vec3& GetRightVector();
+		glm::vec3 GetForwardVector();
+		glm::vec3 GetRightVector();
 
-		const glm::mat4& GetViewMatrix();
-		const glm::mat4& GetProjectionMatrix();
+		glm::mat4 GetViewMatrix();
+		glm::mat4 GetProjectionMatrix();
 
-		inline void SetMoveStraightDelta(float delta) { m_MoveStraightDelta = delta; }
-		inline void SetMoveSidewayDelta(float delta) { m_MoveSidewayDelta = delta; }
-		inline void SetRotateHorizontallyAngle(float angle) { m_RotateHorizontallyAngle = angle; }
-		inline void SetRotateVerticallyAngle(float angle) { m_RotateVerticallyAngle = angle; }
-
-	private:
-		void MoveStraight(float delta);			// positive delta - move forward
-		void MoveSideway(float delta);			// positive delta - move right
-		void RotateHorizontally(float angle);	// positive angle - rotate right
-		void RotateVertically(float angle);		// positive angle - rotate up
+		void MoveForward(float delta);
+		void MoveRight(float delta);
+		
+		void RotateUp(float angle);
+		void RotateRight(float angle);
 
 	private:
 		render::Camera m_Proxy = {};
 		
 		Dirty<glm::mat4> m_ViewMatrix = {};
 		Dirty<glm::mat4> m_ProjectionMatrix = {};
+		
 		Dirty<glm::vec3> m_ForwardVector = {};
 		Dirty<glm::vec3> m_RightVector = {};
 		
 		Type m_Type = Type::Perspective;
-
-		float m_MoveStraightDelta = 0.0f;
-		float m_MoveSidewayDelta = 0.0f;
-		float m_RotateHorizontallyAngle = 0.0f;
-		float m_RotateVerticallyAngle = 0.0f;
 	};
 }
