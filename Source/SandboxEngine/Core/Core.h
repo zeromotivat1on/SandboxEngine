30eb879f37a5a757b96be9bd7c01dc94dd0a12d9
Alexander <lyannoy.alexander@gmail.com>
2024-08-25 12:32:43 +0200

    Bit mask macros.

--- Source/SandboxEngine/Core/Core.h
+++ Source/SandboxEngine/Core/Core.h
@@ -9,7 +9,12 @@
 #define SND_EXPAND_MACRO(x) x
 #define SND_STRINGIFY_MACRO(x) #x
 
-#define BIT(x) (1 << (x))
+#define BIT(x) (1 << (x))										// create bit shifted value
+#define BIT_MASK(pos, n) (((1U << (n)) - 1) << (pos))			// create a mask with 'n' bits set starting from a position
+#define SET_BIT(value, pos) ((value) |= (1U << (pos)))			// set a bit at a specific position
+#define CLEAR_BIT(value, pos) ((value) &= ~(1U << (pos)))		// clear a bit at a specific position
+#define TOGGLE_BIT(value, pos) ((value) ^= (1U << (pos)))		// toggle a bit at a specific position
+#define CHECK_BIT(value, pos) (((value) & (1U << (pos))) != 0)	// check if a bit is set at a specific position
 
 #define GLUE_INNER(a, b) a ## b
 #define GLUE(a, b) GLUE_INNER(a, b)
