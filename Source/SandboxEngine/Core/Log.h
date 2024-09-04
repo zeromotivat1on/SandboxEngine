68dadf733721b88dad60c869183355188beef7f0
Alexander <lyannoy.alexander@gmail.com>
2024-09-04 15:10:26 +0200

    Removed include, method rename.

--- Source/SandboxEngine/Core/Log.h
+++ Source/SandboxEngine/Core/Log.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "sndpch.h"
-#include "spdlog/spdlog.h"
+#include <spdlog/spdlog.h>
 
-#define SND_LOG_TRACE(...)		SPDLOG_LOGGER_TRACE(::snd::Log::GetCoreLogger(), __VA_ARGS__)
-#define SND_LOG_DEBUG(...)		SPDLOG_LOGGER_DEBUG(::snd::Log::GetCoreLogger(), __VA_ARGS__)
-#define SND_LOG_INFO(...)		SPDLOG_LOGGER_INFO(::snd::Log::GetCoreLogger(), __VA_ARGS__)
-#define SND_LOG_WARNING(...)	SPDLOG_LOGGER_WARN(::snd::Log::GetCoreLogger(), __VA_ARGS__)
-#define SND_LOG_ERROR(...)		SPDLOG_LOGGER_ERROR(::snd::Log::GetCoreLogger(), __VA_ARGS__)
-#define SND_LOG_CRITICAL(...)	SPDLOG_LOGGER_CRITICAL(::snd::Log::GetCoreLogger(), __VA_ARGS__)
+#define SND_LOG_TRACE(...)		SPDLOG_LOGGER_TRACE(::snd::Log::CoreLogger(), __VA_ARGS__)
+#define SND_LOG_DEBUG(...)		SPDLOG_LOGGER_DEBUG(::snd::Log::CoreLogger(), __VA_ARGS__)
+#define SND_LOG_INFO(...)		SPDLOG_LOGGER_INFO(::snd::Log::CoreLogger(), __VA_ARGS__)
+#define SND_LOG_WARNING(...)	SPDLOG_LOGGER_WARN(::snd::Log::CoreLogger(), __VA_ARGS__)
+#define SND_LOG_ERROR(...)		SPDLOG_LOGGER_ERROR(::snd::Log::CoreLogger(), __VA_ARGS__)
+#define SND_LOG_CRITICAL(...)	SPDLOG_LOGGER_CRITICAL(::snd::Log::CoreLogger(), __VA_ARGS__)
 
 namespace snd
 {
@@ -17,7 +16,7 @@ namespace snd
 	public:
 		static void Init();
 
-		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& CoreLogger() { return s_CoreLogger; }
 
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
