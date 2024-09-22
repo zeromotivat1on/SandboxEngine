997971bc1e8f21dbf30c63389144095227c39627
Alexander <lyannoy.alexander@gmail.com>
2024-09-22 12:22:31 +0200

    Initial camera component.

---
+++ Source/SandboxEngine/Components/CameraComponent.cpp
@@ -0,0 +1,54 @@
+﻿#include "sndpch.h"
+#include "SandboxEngine/Components/CameraComponent.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+glm::vec3 snd::CameraComponent::ForwardVector() const
+{
+    return glm::normalize(At - Eye);
+}
+
+glm::vec3 snd::CameraComponent::RightVector() const
+{
+    return glm::normalize(glm::cross(ForwardVector(), Up));
+}
+
+glm::mat4 snd::CameraComponent::ViewMatrix() const
+{
+    return glm::lookAt(Eye, At, Up);
+}
+
+glm::mat4 snd::CameraComponent::PerspectiveProjectionMatrix() const
+{
+    return glm::perspective(glm::radians(Fov), Aspect, Near, Far);
+}
+
+glm::mat4 snd::CameraComponent::OrthographicProjectionMatrix() const
+{
+    return glm::ortho(Left, Right, Bottom, Top, Near, Far);
+}
+
+void snd::CameraComponent::Move(const glm::vec3& delta)
+{
+    const glm::vec3 forward = ForwardVector();
+    const glm::vec3 right   = RightVector();
+    At  += (forward * delta.x) + (right * delta.y) + (Up * delta.z);
+    Eye += (forward * delta.x) + (right * delta.y) + (Up * delta.z);
+}
+
+void snd::CameraComponent::OnMouseMove(const glm::vec2& offset, f32 sensitivity)
+{
+    Yaw   += offset.x * sensitivity;
+    Pitch += offset.y * sensitivity;
+    Pitch = std::clamp(Pitch, -89.0f, 89.0f);
+    At = Eye + snd::ForwardVector(Yaw, Pitch);
+}
+
+glm::vec3 snd::ForwardVector(f32 yaw, f32 pitch)
+{
+    const f32 ycos = cosf(glm::radians(yaw)); 
+    const f32 ysin = sinf(glm::radians(yaw));
+    const f32 pcos = cosf(glm::radians(pitch));
+    const f32 psin = sinf(glm::radians(pitch));
+    const glm::vec3 forward = { ycos * pcos, psin, ysin * pcos };
+    return glm::normalize(forward);
+}
---
+++ Source/SandboxEngine/Components/CameraComponent.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <glm/fwd.hpp>
+#include <glm/vec3.hpp>
+
+namespace snd
+{
+    struct CameraComponent
+    {
+        glm::vec3 			At;		                                // point in space the camera is looking at (target position)
+        glm::vec3 			Up;		                                // up vector defining the camera's vertical direction
+        glm::vec3 			Eye;	                                // position of the camera in the world space
+        
+        f32                 Yaw;                                    // horizontal rotation around Y axis
+        f32                 Pitch;                                  // vertical rotation around X axis
+        
+        f32 				Fov;	                                // field of camera view
+        f32 				Aspect;	                                // aspect ratio
+        
+        f32 				Near;	                                // near clipping plane
+        f32 				Far;	                                // far clipping plane
+        f32 				Left;	                                // left clipping plane
+        f32 				Right;	                                // right clipping plane
+        f32 				Bottom;	                                // bottom clipping plane
+        f32 				Top;	                                // top clipping plane
+		
+        glm::vec3			ForwardVector() const;					// calculate forward vector
+        glm::vec3			RightVector() const;					// calculate right vector
+        glm::mat4			ViewMatrix() const;						// calculate view matrix
+        glm::mat4			PerspectiveProjectionMatrix() const;	// calculate perspective projection matrix
+        glm::mat4			OrthographicProjectionMatrix() const;	// calculate orthographic projection matrix
+
+        // Move camera by a given delta distance.
+        void                Move(const glm::vec3& delta);
+        
+        // Update camera rotation based on mouse move input offset.
+        void                OnMouseMove(const glm::vec2& offset, f32 sensitivity);
+    };
+
+    // Convert spherical coordinates (yaw, pitch) to cartesian coordinates (forward vector).
+    glm::vec3 ForwardVector(f32 yaw, f32 pitch);
+}
