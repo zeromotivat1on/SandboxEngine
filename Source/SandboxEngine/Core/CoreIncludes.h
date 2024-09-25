3267b20aae254862c3484cf3a81c765ca5ba5fe9
Alexander <lyannoy.alexander@gmail.com>
2024-09-25 19:07:36 +0200

    Remove dirty include.

--- Source/SandboxEngine/Core/CoreIncludes.h
+++ Source/SandboxEngine/Core/CoreIncludes.h
@@ -3,7 +3,6 @@
 #include "SandboxEngine/Core/CoreMacros.h"
 #include "SandboxEngine/Core/Log.h"
 #include "SandboxEngine/Core/Error.h"
-#include "SandboxEngine/Core/Dirty.h"
 #include "SandboxEngine/Core/Timer.h"
 #include "SandboxEngine/Core/Profile.h"
 #include "SandboxEngine/Core/StringId.h"
