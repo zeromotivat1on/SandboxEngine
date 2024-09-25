c6a6384d3b41f5f4ebc4053d4ca8ecddb24b5c84
Alexander <lyannoy.alexander@gmail.com>
2024-09-25 19:07:06 +0200

    Initial render components (mesh and material).

---
+++ Source/SandboxEngine/Components/MaterialComponent.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <bgfx/bgfx.h>
+
+namespace snd
+{
+    struct MaterialComponent
+    {
+        bgfx::TextureHandle     Texture;
+        bgfx::UniformHandle     Uniform;
+    };
+}
---
+++ Source/SandboxEngine/Components/MeshComponent.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <bgfx/bgfx.h>
+
+namespace snd
+{
+    struct MeshComponent
+    {
+        bgfx::VertexBufferHandle    Vbh;    // vertex buffer handle
+        bgfx::IndexBufferHandle     Ibh;    // index buffer handle
+        bgfx::ProgramHandle         Rph;    // render pipeline handle
+    };
+}
