85df314c6e3d3661103b49643e00bdc8a21fbe8e
Alexander <lyannoy.alexander@gmail.com>
2024-09-25 19:06:25 +0200

    Default render vertex.

---
+++ Source/SandboxEngine/Render/Vertex.cpp
@@ -0,0 +1,15 @@
+﻿#include "sndpch.h"
+#include "SandboxEngine/Render/Vertex.h"
+
+bgfx::VertexLayout snd::Vertex::Layout;
+
+void snd::Vertex::InitLayout()
+{
+    Layout
+        .begin()
+        .add(bgfx::Attrib::Position,  3, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::Normal,    3, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::Color0,    4, bgfx::AttribType::Uint8, true)
+        .end();
+}
---
+++ Source/SandboxEngine/Render/Vertex.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <bgfx/bgfx.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+namespace snd
+{
+    struct Vertex
+    {
+        glm::vec3                   Position;
+        glm::vec3                   Normal;
+        glm::vec2                   Texture;
+        u32                         Color;
+
+        static bgfx::VertexLayout   Layout;
+        static void                 InitLayout();
+    };
+}
