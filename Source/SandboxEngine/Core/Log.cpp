5c8e6b42c3225c548e37f12faa69650bf3dfe492
Alexander <lyannoy.alexander@gmail.com>
2024-10-03 23:59:21 +0200

    Show short log level.

--- Source/SandboxEngine/Core/Log.cpp
+++ Source/SandboxEngine/Core/Log.cpp
@@ -7,6 +7,6 @@ std::shared_ptr<spdlog::logger> snd::Log::s_CoreLogger;
 void snd::Log::Init()
 {
     s_CoreLogger = spdlog::stdout_color_mt("SndCore");
-    s_CoreLogger->set_pattern("%^[%d-%m-%Y] [%T] [%n] [%s:%#]: %v%$");
+    s_CoreLogger->set_pattern("%^[%d-%m-%Y] [%T] [%n:%L] [%s:%#]: %v%$");
     s_CoreLogger->set_level(spdlog::level::trace);
 }
